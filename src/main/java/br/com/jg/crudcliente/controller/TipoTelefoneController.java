e2cebb8dbc9d119f8245c5ed8d106b370532b26f
João Gabriel <desenvolvedorjg@gmail.com>
2020-01-04 14:49:41 -0300

    [FIX] Arrumado problema de cross origin das request (cors) com aplicação frontend.

--- src/main/java/br/com/jg/crudcliente/controller/TipoTelefoneController.java
+++ src/main/java/br/com/jg/crudcliente/controller/TipoTelefoneController.java
@@ -5,10 +5,7 @@ import br.com.jg.crudcliente.repository.TipoTelefoneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -16,6 +13,7 @@ import java.util.Optional;
 
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
+@CrossOrigin
 @RestController
 public class TipoTelefoneController {
 
--- src/main/java/br/com/jg/crudcliente/controller/UsuarioController.java
+++ src/main/java/br/com/jg/crudcliente/controller/UsuarioController.java
@@ -14,6 +14,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 import br.com.jg.crudcliente.entity.Usuario;
 import br.com.jg.crudcliente.repository.UsuarioRepository;
 
+@CrossOrigin
 @RestController
 public class UsuarioController {
 
