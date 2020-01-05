7db62c07fb78fbeec0de3eb872ddf8cd49b079c3
João Gabriel <desenvolvedorjg@gmail.com>
2020-01-04 21:42:14 -0300

    [UPT] Está comentado pois criar o database e em seguidas os scripts não funcionou no primeiro momento.

--- src/main/resources/db/changelog/migrations/001_schema_inicial.sql
+++ src/main/resources/db/changelog/migrations/001_schema_inicial.sql
@@ -1,7 +1,14 @@
 --liquibase formatted sql
 
 --changeset jg:1
--- CREATE DATABASE "crudClienteCooperSys";
+-- CREATE DATABASE "crudClienteCooperSys"
+--     WITH
+--     OWNER = postgres
+--     ENCODING = 'UTF8'
+--     LC_COLLATE = 'Portuguese_Brazil.1252'
+--     LC_CTYPE = 'Portuguese_Brazil.1252'
+--     TABLESPACE = pg_default
+--     CONNECTION LIMIT = -1;
 
 CREATE SEQUENCE perfil_id_perfil_seq;
 
