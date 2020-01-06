aa2d0d008f0790f572705b1b0e16be3ec6f8e48c
João Gabriel <desenvolvedorjg@gmail.com>
2020-01-06 10:51:42 -0300

    [ADD] Adicionado getby por idUsuario.

--- src/main/java/br/com/jg/crudcliente/repository/LogOperacoesRepository.java
+++ src/main/java/br/com/jg/crudcliente/repository/LogOperacoesRepository.java
@@ -2,8 +2,13 @@ package br.com.jg.crudcliente.repository;
 
 import br.com.jg.crudcliente.entity.LogOperacoes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LogOperacoesRepository extends JpaRepository<LogOperacoes, Long> {
+    @Query("SELECT l FROM LogOperacoes l WHERE l.idUsuario = ?1")
+    List<LogOperacoes> findByIdUsuario(long idUsuario);
 }
